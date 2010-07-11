41d601b23e41663abb4fb7111d0f57aa5b3f8997
Jan Schwenzien <jan@schwenzien.info>
2010-07-11 12:43:09 +0200

    changed gem versions, added seed_fu rake task, added formtastic initializer, other minor fixes

--- base.rb
+++ base.rb
@@ -28,10 +28,6 @@ def file_str_replace(file_name, sentinel, replacement)
   end
 end
 
-if 'true' == ENV['INTEGRITY']
-  sudo = 'y'
-end
-
 # Git
 file_append '.gitignore', open("#{SOURCE}/gitignore").read
 git :init
@@ -48,20 +44,20 @@ file 'Gemfile', <<-EOGEMS
 source 'http://rubygems.org'
 source 'http://gems.github.com'
 
-gem 'rails', '3.0.0.beta4'
-gem 'authlogic', :git => 'git://github.com/binarylogic/authlogic.git'
+gem 'rails', '>=3.0.0.beta4'
+gem 'authlogic'
 gem 'capistrano', '2.5.19'
 gem 'capistrano-ext', '1.2.1'
-gem 'config_reader', '0.0.7'
-gem 'friendly_id', '3.0.6'
+gem 'config_reader', '>=0.0.7'
+gem 'friendly_id', '>=3.0.6'
 gem "formtastic", :git => "git://github.com/justinfrench/formtastic.git", :branch => "rails3"
-gem 'haml', '3.0.13'
-gem 'hoe', '2.6.1'
+gem 'haml', '>=3.0.13'
+gem 'hoe', '>=2.6.1'
 gem 'mongrel'
 gem 'mysql'
-gem 'seed-fu', '1.2.3', :git => 'http://github.com/mixonic/seed-fu.git'
-gem 'test-unit', '2.0.9'
-gem 'will_paginate', '2.3.14'
+gem 'seed-fu', :git => 'git://github.com/mixonic/seed-fu.git'
+gem 'test-unit', '>=2.0.9'
+gem 'will_paginate', '>=2.3.14'
 
 group :test do
   gem 'capybara'
@@ -93,8 +89,10 @@ rakefile 'strappy.rake', open("#{SOURCE}/lib/tasks/strappy.rake").read
 # # install haml rake tasks
 # rakefile 'haml.rake', open("#{SOURCE}/lib/tasks/haml.rake").read
 # 
-# # install seed_fu rake tasks
-# rakefile 'seed_fu.rake', open("#{SOURCE}/lib/tasks/seed_fu.rake").read
+
+# install seed_fu rake tasks
+rakefile 'seed_fu.rake', open("#{SOURCE}/lib/tasks/seed_fu.rake").read
+
 # 
 # # install rcov rake tasks
 # rakefile 'rcov.rake', open("#{SOURCE}/lib/tasks/rcov.rake").read
@@ -109,23 +107,9 @@ git :commit => "-am 'Added RSpec'"
 # Cucumber
 gen 'cucumber:install --rspec --capybara'
 
-file_str_replace('config/cucumber.yml', 
-  %q{rerun = File.file?('rerun.txt') ? IO.read('rerun.txt') : ""},
-  %q{rerun = ""}
-)
-
-# file_inject('/spec/spec_helper.rb',
-#   "require 'authlogic/test_case'",
-#   "require 'webrat'
-# 
-# Webrat.configure do |config|
-#   config.mode = :rails
-# end",
-#   :after
-# )
-
 git :add => "."
 git :commit => "-am 'Added Cucumber'"
+
 
 # 
 # SiteConfig
@@ -184,7 +168,7 @@ plugin 'jrails', :git => 'git://github.com/jeanmartin/jrails.git'
 gen 'jrails'
 rake('jrails:js:scrub')
 
-file 'config/initializers/jrails.rb', <<-EOF
+initializer 'jrails.rb', <<-EOF
 ActionView::Helpers::PrototypeHelper::DISABLE_JQUERY_FORGERY_PROTECTION = true
 EOF
 
@@ -214,6 +198,62 @@ git :commit => "-am 'Added Blackbird'"
 
 # Formtastic
 gen 'formtastic:install'
+initializer 'formtastic.rb', <<-CODE
+# Set the default text field size when input is a string. Default is 50.
+# Formtastic::SemanticFormBuilder.default_text_field_size = 50
+
+# Set the default text area height when input is a text. Default is 20.
+# Formtastic::SemanticFormBuilder.default_text_area_height = 5
+
+# Should all fields be considered "required" by default?
+# Defaults to true, see ValidationReflection notes below.
+# Formtastic::SemanticFormBuilder.all_fields_required_by_default = true
+
+# Should select fields have a blank option/prompt by default?
+# Defaults to true.
+# Formtastic::SemanticFormBuilder.include_blank_for_select_by_default = true
+
+# Set the string that will be appended to the labels/fieldsets which are required
+# It accepts string or procs and the default is a localized version of
+# '<abbr title="required">*</abbr>'. In other words, if you configure formtastic.required
+# in your locale, it will replace the abbr title properly. But if you don't want to use
+# abbr tag, you can simply give a string as below
+# Formtastic::SemanticFormBuilder.required_string = "(required)"
+
+# Set the string that will be appended to the labels/fieldsets which are optional
+# Defaults to an empty string ("") and also accepts procs (see required_string above)
+# Formtastic::SemanticFormBuilder.optional_string = "(optional)"
+
+# Set the way inline errors will be displayed.
+# Defaults to :sentence, valid options are :sentence, :list and :none
+# Formtastic::SemanticFormBuilder.inline_errors = :sentence
+
+# Set the method to call on label text to transform or format it for human-friendly
+# reading when formtastic is used without object. Defaults to :humanize.
+# Formtastic::SemanticFormBuilder.label_str_method = :humanize
+
+# Set the array of methods to try calling on parent objects in :select and :radio inputs
+# for the text inside each @<option>@ tag or alongside each radio @<input>@. The first method
+# that is found on the object will be used.
+# Defaults to ["to_label", "display_name", "full_name", "name", "title", "username", "login", "value", "to_s"]
+# Formtastic::SemanticFormBuilder.collection_label_methods = [
+#   "to_label", "display_name", "full_name", "name", "title", "username", "login", "value", "to_s"]
+
+# Formtastic by default renders inside li tags the input, hints and then
+# errors messages. Sometimes you want the hints to be rendered first than
+# the input, in the following order: hints, input and errors. You can
+# customize it doing just as below:
+# Formtastic::SemanticFormBuilder.inline_order = [:input, :hints, :errors]
+
+# Specifies if labels/hints for input fields automatically be looked up using I18n.
+# Default value: false. Overridden for specific fields by setting value to true,
+# i.e. :label => true, or :hint => true (or opposite depending on initialized value)
+Formtastic::SemanticFormBuilder.i18n_lookups_by_default = true
+
+# You can add custom inputs or override parts of Formtastic by subclassing SemanticFormBuilder and
+# specifying that class here.  Defaults to SemanticFormBuilder.
+# Formtastic::SemanticFormHelper.builder = MyCustomBuilder
+CODE
 
 
 # Setup Authlogic
